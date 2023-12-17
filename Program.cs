32e35d6347f371ae102234b8e8a39bcdf23874dd
Gabriel Torres <gaabrieltt7@gmail.com>
2023-12-17 20:17:18 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using AbstracaoCelularPOO.Models;
+
+Console.WriteLine("\niPhone \n");
+
+var iphone = new Iphone("81999999999", "13 Pro Max", "000000-00-000000-0", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("ZapZap");
+iphone.ReceberLigacao();
+
+Console.WriteLine("\nNokia \n");
+
+var nokia = new Nokia("81999999999", "Nokia Tijolão", "12345678964336", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+nokia.ReceberLigacao();
